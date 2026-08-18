9edc07223c2f9d012b1fc2c95c921eea42147d0a
Mateus <mateuslimasesi@gmail.com>
2026-08-18 16:28:02 -0300

    Add files via upload

---
+++ vetores.py
@@ -0,0 +1,7 @@
+numeros = [33, 32, 34, 50, 55]
+nomes = ['Mateus', 'João', 'Maria']
+
+#append - Insere um novo valor sempre na última posição.
+#insert - Insere um novo valor numa posição previamente selecionada.
+#pop - Deleta um valor pela sua posição
+#remove - Deleta um valor pelo seu conteúdo
