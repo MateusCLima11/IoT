cd750792ce3ca20422cca3e038c889f5cb208b55
Mateus <mateuslimasesi@gmail.com>
2026-08-19 14:55:17 -0300

    Add files via upload

--- vetores.py
+++ vetores.py
@@ -1,7 +1,17 @@
-numeros = [33, 32, 34, 50, 55]
+numeros = [33, 32, 34, 50, 55, 18, 27, 33]
 nomes = ['Mateus', 'João', 'Maria']
 
-#append - Insere um novo valor sempre na última posição.
-#insert - Insere um novo valor numa posição previamente selecionada.
-#pop - Deleta um valor pela sua posição
-#remove - Deleta um valor pelo seu conteúdo
+maior = max(numeros)
+menor = min(numeros)
+print(f'O maior é {maior} e o menor é {menor}')
+
+
+# append - Insere um novo valor sempre na última posição.
+# insert - Insere um novo valor numa posição previamente selecionada.
+# pop - Deleta um valor pela sua posição.
+# remove - Deleta um valor pelo seu conteúdo.
+# sort- ordena o vetor.
+# reverse - iverte as posições do vetor.
+# len - conta quantos objetos tem armazenado dentro de um vetor.
+# count - Informa a quantidade de um valor específico dentro de um vetor.
+# sum - Soma todos os valores de um vetor.
